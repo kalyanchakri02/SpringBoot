a87258ec08eeabfd9db1241212c871df55594e20
kalya <kalyanchakri02@gmail.com>
2017-04-20 18:28:10 -0400

    commiting the whole project files

---
+++ src/main/java/com/controller/UploadController.java
@@ -0,0 +1,45 @@
+package com.controller;
+
+import java.awt.PageAttributes.MediaType;
+import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class UploadController {
+
+private final static Logger log = (Logger) LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+// first we need to save an uploaded video file
+            @Value("${multipart.location}")
+            private String location;
+// second we have to give the time spans of the gif
+             
+            @RequestMapping(value= "/upload", method=RequestMethod.POST, produces=org.springframework.http.MediaType.IMAGE_GIF_VALUE)
+            public  String upload(@RequestPart("file") MultipartFile file,
+            		             @RequestParam("start") int start,
+            		             @RequestParam("end") int end,
+            		             @RequestParam("speed") int speed,
+            		             @RequestParam("repeat") int repeat) throws IllegalStateException, IOException{
+           File videoFile =  new File("location"+"/"+System.currentTimeMillis()+".mp4");
+           file.transferTo(videoFile);
+           
+           
+           return "";
+            }
+            		             
+//finally we have to download the file
+
+
+	
+}
---
+++ src/main/java/com/kalyan/inturi/justGifApplication.java
@@ -0,0 +1,11 @@
+package com.kalyan.inturi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class justGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(justGifApplication.class, args);
+	}
+}
